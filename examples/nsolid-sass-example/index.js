a04602109e66fec927ffbc6696af75da4225b134
Erick Wendel <erick.workspace@gmail.com>
2021-11-12 15:24:05 -0300

    fix: linting problems
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- examples/nsolid-sass-example/index.js
+++ examples/nsolid-sass-example/index.js
@@ -1,5 +1,5 @@
 const { createServer } = require('http')
 
 createServer((request, response) => {
-    response.end('Hello World')
+  response.end('Hello World')
 }).listen(3000, _ => console.log('server running at 3000'))
